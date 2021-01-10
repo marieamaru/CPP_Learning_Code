05a3258d6bf64591938655628ceabc93438cbc05
mamaru1 <mamaru@etud.u-pem.fr>
2021-01-10 17:14:54 +0100

    parrot

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,13 @@
-// Implement the program here.
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    for (int i = 0; i < 10; ++i)
+    {
+        string word;
+        cin >> word;
+        cout << "Craow " << word << "!" << std::endl;
+    }
+    return 0;
+}
