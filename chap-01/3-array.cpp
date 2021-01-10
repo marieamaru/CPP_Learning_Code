321c037dfe24f3486556760248e92aed12dc6b5e
mamaru1 <mamaru@etud.u-pem.fr>
2021-01-10 18:00:35 +0100

    array

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-
-int main()
+#include <vector>
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
+    using namespace std;
+    if(argc<2)
     {
-        std::cout << array[i] << std::endl;
+        cerr << "Expected argument for array size." << std::endl;
+        return -1;
     }
 
+    int length = stoi(argv[1]);
+    if (length <= 0)
+    {
+        cerr << "Expected strictly positive value for array size." << std::endl;
+        return -1;
+    }
+
+    vector<int> array;
+
+    for (int i = 0; i<length; i++)
+    {
+        array.emplace_back(i+1);
+    }
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
     return 0;
 }
