1d61426c43f0e3f05c387b312caffdf52ff635c6
mamaru1 <mamaru@etud.u-pem.fr>
2021-01-16 14:23:08 +0100

    try

--- chap-02/2-constructor.cpp
+++ chap-02/2-constructor.cpp
@@ -4,25 +4,29 @@
 class Person
 {
 public:
-    const std::string& get_name() const { return _name; }
+    Person(const std::string& name, const std::string& surname)
+        : _name { name }
+        , _surname { surname }
+    {}
+
+    std::string get_full_name() const { return _name +" "+ _surname; }
     unsigned int       get_age() const  { return _age; }
 
-    void set_name(const std::string& name) { _name = name; }
-    void set_age(unsigned int age)         { _age = age; }
+    void wait(unsigned int years) { _age += years; }
 
 private:
     std::string  _name;
+    std::string  _surname;
     unsigned int _age = 0u;
 };
 
 int main()
 {
-    Person p;
+    Person p { "Bruce" , "Wayne" };
 
-    p.set_name("Batman");
     p.set_age(23);
 
-    std::cout << "Person named '" << p.get_name() << "' is " << p.get_age() << " years old." << std::endl;
+    std::cout << "Person named '" << p.get_full_name() << "' is " << p.get_age() << " years old." << std::endl;
 
     return 0;
 }
--- chap-02/3-clone.cpp
+++ chap-02/3-clone.cpp
@@ -8,10 +8,26 @@ public:
         : _name { name }, _surname { surname }
     {}
 
+    Person(const Person& other)
+        : _name { other._name }
+        , _surname { other._surname }
+    {
+
+    }
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const  { return _age; }
 
     void wait(unsigned int years) { _age += years; }
+
+    Person& operator=(const Person& other)
+    {
+        if (this != &other)
+        {
+            _name = other._name;
+            _surname = other._surname;
+        }
+        return *this;
+    }
 
 private:
     std::string  _name;
@@ -19,12 +35,26 @@ private:
     unsigned int _age = 0u;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Person& person)
+{
+    return stream << "Person named '" << person.get_full_name() << "' is " << person.get_age() << " years old.";
+}
+
+
 int main()
 {
     Person batman { "Bruce", "Wayne" };
     batman.wait(23);
 
-    std::cout << "Person named '" << batman.get_full_name() << "' is " << batman.get_age() << " years old." << std::endl;
+    std::cout << batman << std::endl;
+    Person clone = Person(batman);
+    std::cout << clone << std::endl;
+
+    Person assigned_clone { "Batman", "2" };
+    std::cout << assigned_clone << std::endl;
+
+    assigned_clone = batman;
+    std::cout << assigned_clone << std::endl;
 
     return 0;
 }
